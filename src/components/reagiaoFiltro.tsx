05b57406d87d9226a4aea0ba6761bf4576a2870b
Adão com H <adaocomh@gmail.com>
2025-04-26 13:54:18 -0300

    Atualiza componente Filtro para gerenciar a exibição de regiões e ajusta estilo do input na Barra de Pesquisa

--- src/components/reagiaoFiltro.tsx
+++ src/components/reagiaoFiltro.tsx
@@ -1,8 +1,22 @@
+'use client'
+import React, { useState } from "react"
 export default function Filtro(){
+    const [regioes, setRegioes] = useState(false)
+
     return(
-        <div className="flex justify-between rounded-[5px] shadow-md/10 p-[15px] w-[250px] text-gray-500 font-light">
+        <div className="flex flex-col relative">
+        <div className="flex justify-between rounded-[5px] shadow-md/15 p-[15px] w-[250px] text-gray-500 font-light" onClick={() => setRegioes(!regioes)}>
             <p>Filtrar Por região</p>
-            <p>⌄</p>
+            <p>{regioes ? '⌃' : '⌄'}</p>
+        </div>
+        <ul className={`${regioes ? "block" : "hidden"} absolute top-[70px] bg-white p-[10px] rounded-[5px] shadow-md/15 w-[250px] text-gray-500 font-light`}>
+            <li className="p-[8px] hover:text-shadow-lg">America</li>
+            <li className="p-[8px] hover:text-shadow-lg">Ásia</li>
+            <li className="p-[8px] hover:text-shadow-lg">Europa</li>
+            <li className="p-[8px] hover:text-shadow-lg">África</li>
+            <li className="p-[8px] hover:text-shadow-lg">Oceania</li>
+            <li className="p-[8px] hover:text-shadow-lg">Antártida</li>
+        </ul>
         </div>
     )
 }
