cd1a1a83427d3dc86991a425f9dbabc55837611c
Adão com H <adaocomh@gmail.com>
2025-04-27 09:04:06 -0300

    Ajustes de cursor no "dark mode".

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ export default function Header(){
         <header>
             <div className="flex justify-between items-center h-[80px] p-[40px] bg-white text-black border-b-[1px] border-[#D9D9D9] shadow-md">
                 <h1 className="text-[22px]">Onde no mundo?</h1>
-                <button className="font-light">☽ Dark mode</button>
+                <button className="font-light cursor-pointer w-max h-max">☽ Dark mode</button>
             </div>
         </header>
     )
