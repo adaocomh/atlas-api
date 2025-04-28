fd23fcd9f62f166e5a1c1af64984c04b9035dc6b
Adão com H <adaocomh@gmail.com>
2025-04-28 09:47:40 -0300

    Exibindo cards...
    Os cads foram exibidos na pagina main.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+
 import Image from "next/image";
 import Header from "@/components/header";
 import SearchAndFilter from "./../components/searchAndFilter/searchFilter"
+import PaisCard from "../components/paisCard";
 
 export default function Home() {
   return (
     <>
     <Header/>
     <SearchAndFilter/>
+    <PaisCard/>
+
     </>
   );
 }
--- src/components/paisCard.tsx
+++ src/components/paisCard.tsx
@@ -1,13 +1,20 @@
-export default function PaisCard(){
+import data from '../components/data/data.json'
+
+export default async function PaisCard(){
     return(
-        <div>
-            <img>Imagem do pais</img>
-            <div>
-                <h2>Nome do pais</h2>
-                <p>População: 100000</p>
-                <p>Região: América do Sul</p>
-                <p>Capital: Brasília</p>
-            </div>
-        </div>
+    <div className='grid grid-cols-4 gap-4 p-[40px]'>
+        {data.map((pais: any) =>(
+            <div key={pais.name} className='bg-white shadow-md rounded-[5px]'>
+                <img src={pais.flags.png} className='rounded-t-[5px] '></img>
+                <div>
+                    <ul className='p-[20px]'>
+                    <li className='p-[3px] font-black'>{pais.name}</li>
+                    <li className='p-[3px] font-semibold'>População: <span className='font-thin'>{pais.population}</span></li>
+                    <li className='p-[3px] font-semibold'>Região: <span className='font-thin'>{pais.region}</span></li>
+                    <li className='p-[3px] font-semibold'>Capital: <span className='font-thin'>{pais.capital}</span></li>
+                    </ul>
+                </div>
+            </div>))}
+    </div>
     )
 }
