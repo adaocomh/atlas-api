105d5d8c5fdde7fd53a3c3e38b2473a1e7fe1f6d
Adão com H <adaocomh@gmail.com>
2025-05-12 13:51:44 -0300

    PNGs dos modos atualizados.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react"
 import { useTheme } from "next-themes"
 import Link from "next/link"
+import Image from "next/image"
 export default function Header(){
     const { theme, setTheme } = useTheme()
     const [mounted, setMounted] = useState(false)
@@ -12,10 +13,18 @@ export default function Header(){
     if(!mounted) return null
     return(
         <header>
-            <div className="flex justify-between items-center dark:bg-[var(--cards)]  dark:border-none dark:shadow-[0px_10px_10px_rgba(0,0,0,1)] h-[80px] p-[40px] bg-[#ece6e1] text-black text-[16px]  border-b-[1px] border-[#D9D9D9]  shadow-md">
+            <div className="flex justify-between items-center dark:bg-[var(--cards)]  dark:border-none dark:shadow-[0px_10px_10px_rgba(0,0,0,1)] h-[80px] p-[40px] bg-[#ece6e1] text-[16px]  border-b-[1px] border-[#D9D9D9]  shadow-md">
                 <Link href={'/'}>
                 <h1 className="dark:text-[#BFB2A3] hover:translate-y-[-1px] hover:text-shadow-lg transition-all duration-150 text-shadow-md text-[22px] text-[#594834]">Onde no mundo?</h1></Link>
-                <button onClick={() => setTheme(theme ==='light' ? 'dark' : 'light')} className="dark:text-[#BFB2A3] hover:translate-y-[-1px] hover:text-shadow-lg transition-all duration-150 text-shadow-md font-light text-[#594834] cursor-pointer w-max h-max ">{theme === 'dark' ? '☼ Light Mode' : '☽ Dark mode'}</button>
+                <button onClick={() => setTheme(theme ==='light' ? 'dark' : 'light')} className="dark:text-[#BFB2A3] hover:translate-y-[-1px] hover:text-shadow-lg transition-all duration-150 text-shadow-md font-light text-[#594834] cursor-pointer w-max h-max ">{theme === 'dark' ? (
+                    <div className="flex gap-[10px] items-center">
+                    <Image src="/icons/night-mode_bege.png" alt="" width={30} height={20}/> <p>Light Mode</p>
+                    </div>
+                    ) : (
+                    <div className="flex gap-[10px] items-center">
+                    <Image src="/icons/light-mode_brown.png" alt="" width={30} height={20}/>Dark mode
+                    </div>
+                    )}</button>
             </div>
         </header>
     )
