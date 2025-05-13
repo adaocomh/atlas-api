1ea60741eefb47e2d740031fa3addcfcd7dd621d
Adão com H <adaocomh@gmail.com>
2025-05-13 07:56:00 -0300

    Mobile responsive.
    Responsividade para dispositivo mobile ajustada.

--- src/components/searchAndFilter/page.tsx
+++ src/components/searchAndFilter/page.tsx
@@ -9,7 +9,7 @@ export default function SearchFilter(){
     const [regiao, setRegiao] = useState('')
     return(
         <div className="flex flex-col dark:text-[#BFB2A3]">
-            <div className="flex flex-col items-start gap-[40px] sm:flex sm:flex-row sm:justify-between sm:items-center p-[60px]">
+            <div className="flex flex-col items-start gap-[5vh] sm:flex sm:flex-row sm:justify-between sm:items-center md:p-[5vw] p-[10vw]">
                 <Search/>
                 <RegiaoFiltrada onRegiaoClick={setRegiao}/>
             </div>
