5ce4ebfc6ce75e972a2984c2805db9d4df539d2a
Adão com H <adaocomh@gmail.com>
2025-05-19 20:42:51 -0300

    Refatoração da interface PageProps para utilizar Promise e melhorar a extração do nome

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -1,13 +1,13 @@
+import { use } from 'react'
 import PaisCard from "./PaisCardIndv"
 
-interface PageProps{
-    params: {
-        name: string
-    }
+interface PageProps {
+  params: Promise<{ name: string }>
 }
 
-export default function NomeFiltrado({ params }:  PageProps ) {
-    const decodeName = decodeURIComponent(params.name)
+export default function NomeFiltrado({ params }: PageProps){
+    const { name } = use(params)
+    const decodeName = decodeURIComponent(name)
     return(
         <div>
             <PaisCard name={decodeName}/>
